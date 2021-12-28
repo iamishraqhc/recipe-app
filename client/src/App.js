ec9926daef6debdf59caa31e2e9fa0fc634cc995
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2021-12-28 21:28:20 +0100

    changed the structure of Recipe in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,58 @@
-import React, {useEffect, useState} from 'react'
-import Recipe from './Recipe';
-import axios from 'axios'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
-  const [recipes, setRecipes] = useState([])
-  const [search, setSearch] = useState('')
-  const [query, setQuery] = useState('chicken')
-
-  useEffect( () => {
-    getRecipes()
-  }, [query])
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await axios.get(`http://localhost:5000/recipes/${query}`)
-    console.log(response.data)
-    setRecipes(response.data)
+    const response = await axios.get(`http://localhost:5000/recipes/${query}`);
+    console.log(response.data);
+    setRecipes(response.data);
   };
 
-  const updateSearch = e => {
-    setSearch(e.target.value)
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
   };
 
-  const getSearch = e => {
-    e.preventDefault()
-    setQuery(search)
-    setSearch('')
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
   };
 
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
-      <div className='recipes'>
-        {recipes.map(recipe =>(
-          <Recipe key={recipe.recipe.label} title={recipe.recipe.label} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
         ))}
       </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
